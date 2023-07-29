caa17069d275390c41bf072d4d1c1be3fa9423e5
Charan K <charankulal0241@gmail.com>
2023-07-29 14:22:12 +0530

    Create solution.py

---
+++ Python/Print Function/solution.py
@@ -0,0 +1,5 @@
+# Qustion Link:  https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+n=int(input())
+for i in range(1,n+1):
+    print(i,end='')
