03ffa646a7b970836e4e9b0a2674cf8f492ee839
Charan K <charankulal0241@gmail.com>
2023-07-29 22:25:10 +0530

    Create Solution.py

---
+++ Python/Sum and Prod/Solution.py
@@ -0,0 +1,6 @@
+# Question Link : https://www.hackerrank.com/challenges/np-sum-and-prod/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import numpy
+N, M = map(int, input().split())
+A = numpy.array([input().split() for _ in range(N)],int)
+print(numpy.prod(numpy.sum(A, axis=0), axis=0))
