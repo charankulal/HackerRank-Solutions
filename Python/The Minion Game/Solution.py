2d220492b475797c9fca6142f73992baadb0c22e
Charan K <charankulal0241@gmail.com>
2023-07-29 22:35:20 +0530

    Create Solution.py

---
+++ Python/The Minion Game/Solution.py
@@ -0,0 +1,22 @@
+# Question Link : https://www.hackerrank.com/challenges/the-minion-game/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+def minion_game(string):
+    vowels = 'AEIOU'
+    Stuart_score, Kevin_score = 0, 0
+    length = len(string)
+    for start_idx in range(length):
+        score = length - start_idx
+        if string[start_idx] in vowels:
+            Kevin_score += score
+        else:
+            Stuart_score += score
+    if Stuart_score == Kevin_score:
+        print('Draw')
+    if Stuart_score > Kevin_score:
+        print('Stuart {}'.format(Stuart_score))
+    if Stuart_score < Kevin_score:
+        print('Kevin {}'.format(Kevin_score))
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
