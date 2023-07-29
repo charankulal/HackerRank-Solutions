00442ef3722781cd53b4a706280e33bd14275154
Charan K <charankulal0241@gmail.com>
2023-07-29 22:13:25 +0530

    Create Solution.py

---
+++ Python/Floor, Ceil and Rint/Solution.py
@@ -0,0 +1,11 @@
+# Question Link :: https://www.hackerrank.com/challenges/floor-ceil-and-rint/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import numpy
+
+numpy.set_printoptions(legacy='1.13')
+
+arr=numpy.array(input().split(),float)
+
+print(numpy.floor(arr))
+print(numpy.ceil(arr))
+print(numpy.rint(arr))
