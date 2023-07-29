695c1a94e3a4c5344d13cd1c0fde3b9c269d0340
Charan K <charankulal0241@gmail.com>
2023-07-29 21:56:08 +0530

    Create Solution.py

---
+++ Python/Eye and Identity/Solution.py
@@ -0,0 +1,11 @@
+# Question Link: https://www.hackerrank.com/challenges/np-eye-and-identity/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import numpy
+
+numpy.set_printoptions(legacy='1.13')
+
+n,m=map(int,input().split())
+
+print(numpy.eye(n,m,k=0))
+
+
