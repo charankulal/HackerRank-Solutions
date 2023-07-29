8740febfc9c5ab502f5a105f9db209d685319a66
Charan K <charankulal0241@gmail.com>
2023-07-29 21:46:08 +0530

    Create Solution.py

---
+++ Python/Zeros and Ones/Solution.py
@@ -0,0 +1,7 @@
+# Question Link : https://www.hackerrank.com/challenges/np-zeros-and-ones/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import numpy
+
+n = tuple(map(int, input().split()))
+print (numpy.zeros(n, dtype = int))
+print (numpy.ones(n, dtype = int))
