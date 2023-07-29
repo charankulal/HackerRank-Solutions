244c8c7386fa62fd39602fb465330c012bce876a
Charan K <charankulal0241@gmail.com>
2023-07-29 13:57:19 +0530

    Create Solution.java

---
+++ Java/Java Method Overriding/Solution.java
@@ -0,0 +1,39 @@
+// Question Link: https://www.hackerrank.com/challenges/java-method-overriding/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import java.util.*;
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    @Override
+    void getNumberOfTeamMembers()
+    {
+         System.out.println( "Each team has 11 players in " + getName() );
+    }
+
+}
+
+public class Solution{
+	
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+	}
+}
