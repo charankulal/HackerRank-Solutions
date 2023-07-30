d5da0d9a0c3692dbbe461bf2ce4c8408d293d633
Charan K <charankulal0241@gmail.com>
2023-07-30 11:30:04 +0530

    Create Solution.java

---
+++ Java/Java BigDecimal/Solution.java
@@ -0,0 +1,30 @@
+// Question Link: https://www.hackerrank.com/challenges/java-bigdecimal/problem?isFullScreen=true
+
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+   public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+        for(int i=1; i<n ; i++){
+            for(int j=i; j>=1; j--){
+                 if(new BigDecimal(s[j]).compareTo(new BigDecimal(s[j-1]))>0){   
+                    String temp = s[j];
+                    s[j] = s[j-1];
+                    s[j-1] = temp;    
+                }else{
+                    break;
+                }
+            }  
+        } 
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
