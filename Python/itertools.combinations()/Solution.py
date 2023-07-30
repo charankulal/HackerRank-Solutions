499f81d816f4838750708a059a31bd25ebf83b7c
Charan K <charankulal0241@gmail.com>
2023-07-30 13:39:28 +0530

    Create Solution.py

---
+++ Python/itertools.combinations()/Solution.py
@@ -0,0 +1,16 @@
+# Question Link :
+
+# Python 3
+
+
+
+import itertools
+
+word, number_comb = input().split(" ")
+word = sorted(word)
+
+
+for i in range(1,int(number_comb) +1):
+    list_comb = list(itertools.combinations(word,i))
+    for j in list_comb: 
+        print("".join(j))
