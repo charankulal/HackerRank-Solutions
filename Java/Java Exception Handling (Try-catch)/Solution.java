4bd4a82a7f781db9f948b3875306d0293985c9c4
Charan K <charankulal0241@gmail.com>
2023-07-30 12:53:14 +0530

    Create Solution.java

---
+++ Java/Java Exception Handling (Try-catch)/Solution.java
@@ -0,0 +1,26 @@
+//Question Link: https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem?h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+//Java 15
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        try {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(a/b);            
+        } catch (java.util.InputMismatchException ime) {
+            System.out.println("java.util.InputMismatchException");
+        } catch (java.lang.ArithmeticException ae) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        }        
+    }
+}
