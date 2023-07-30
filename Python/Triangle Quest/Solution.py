61067e6b831cf969a6b0c8b66343ab2bf0822795
Charan K <charankulal0241@gmail.com>
2023-07-30 13:31:09 +0530

    Create Solution.py

---
+++ Python/Triangle Quest/Solution.py
@@ -0,0 +1,9 @@
+# Question Link : https://www.hackerrank.com/challenges/python-quest-1/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+# Python 3
+
+
+
+for i in range(1,int(input())): 
+    print( int((i*(pow(10, i) - 1)) / 9 )) 
+    
