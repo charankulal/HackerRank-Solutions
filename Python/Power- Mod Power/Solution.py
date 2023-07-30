8dbde91f452b51cd7e0d761a046fc28252baa0e2
Charan K <charankulal0241@gmail.com>
2023-07-30 13:21:43 +0530

    Create Solution.py

---
+++ Python/Power- Mod Power/Solution.py
@@ -0,0 +1,10 @@
+# Question Link : https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+# Python 3
+
+a=int(input())
+b=int(input())
+m=int(input())
+
+print(pow(a,b))
+print(pow(a,b,m))
