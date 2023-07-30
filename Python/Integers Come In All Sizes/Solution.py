5541cb0a115aad6767bb2d91fb2d691e72b126ef
Charan K <charankulal0241@gmail.com>
2023-07-30 13:27:00 +0530

    Create Solution.py

---
+++ Python/Integers Come In All Sizes/Solution.py
@@ -0,0 +1,10 @@
+# Question Link : https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+# Python 3
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+
+print(pow(a,b)+pow(c,d))
