f2459e2bf8a2708c72599213635ebd684139e499
Charan K <charankulal0241@gmail.com>
2023-07-30 23:03:42 +0530

    Create Solution.sql

---
+++ SQL/Weather Observation Station 12/Solution.sql
@@ -0,0 +1,15 @@
+select distinct city from station
+where not(city like 'A%'
+or city like 'E%'
+or city like 'I%'
+or city like 'O%'
+or city like 'U%')
+and not
+(
+city like '%a'
+or city like '%e'
+or city like '%i'
+or city like '%o'
+or city like '%u'
+);
+
