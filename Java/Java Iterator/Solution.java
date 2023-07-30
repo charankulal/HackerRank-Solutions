96c59187d96cddca786141dd3079b83ef3b8f207
Charan K <charankulal0241@gmail.com>
2023-07-30 12:40:36 +0530

    Create Solution.java

---
+++ Java/Java Iterator/Solution.java
@@ -0,0 +1,41 @@
+//Question Link : https://www.hackerrank.com/challenges/java-iterator/problem?h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+//Java 15
+
+
+import java.util.*;
+public class Main{
+    
+   static Iterator func(ArrayList mylist){
+      Iterator it=mylist.iterator();
+      while(it.hasNext()){
+         Object element = it.next();
+if(element instanceof String)
+
+            break;
+        }
+      return it;
+      
+   }
+   @SuppressWarnings({ "unchecked" })
+   public static void main(String []args){
+      ArrayList mylist = new ArrayList();
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      for(int i = 0;i<n;i++){
+         mylist.add(sc.nextInt());
+      }
+      
+      mylist.add("###");
+      for(int i=0;i<m;i++){
+         mylist.add(sc.next());
+      }
+      
+      Iterator it=func(mylist);
+      while(it.hasNext()){
+         Object element = it.next();
+         System.out.println((String)element);
+      }
+   }
+}
