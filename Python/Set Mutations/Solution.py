0d344a9ed1e568d6ac82262428353ec67386877e
Charan K <charankulal0241@gmail.com>
2023-07-31 11:13:24 +0530

    Create Solution.py

---
+++ Python/Set Mutations/Solution.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    (_, A) = (int(input()),set(map(int, input().split())))
+    B = int(input())
+    for _ in range(B):
+        (command, newSet) = (input().split()[0],set(map(int, input().split())))
+        getattr(A, command)(newSet)
+
+    print (sum(A))
