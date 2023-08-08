da78723490e61a14fcca9926812481d4b56edd17
Charan K <charankulal0241@gmail.com>
2023-08-08 14:26:22 +0530

    Create Solution.py

---
+++ Python/Calendar Module/Solution.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import datetime
+import calendar
+
+m, d, y = map(int, input().split())
+input_date = datetime.date(y, m, d)
+print(calendar.day_name[input_date.weekday()].upper())
