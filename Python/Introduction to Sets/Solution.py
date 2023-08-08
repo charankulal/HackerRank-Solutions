5eac61ba307884ae7a9fe13b2f25fb24adf0e0c2
Charan K <charankulal0241@gmail.com>
2023-08-08 14:25:00 +0530

    Create Solution.py

---
+++ Python/Introduction to Sets/Solution.py
@@ -0,0 +1,9 @@
+def average(array):
+    distinct_heights = set(array)
+    return sum(distinct_heights) / len(distinct_heights)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
