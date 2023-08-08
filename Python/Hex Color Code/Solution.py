ffa6a6f6c5143ad4522dc3645c8c4921f39984d2
Charan K <charankulal0241@gmail.com>
2023-08-08 13:34:07 +0530

    Create Solution.py

---
+++ Python/Hex Color Code/Solution.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+N=int(input())
+
+for i in range(0,N):
+    s=input()
+
+    x=s.split()
+
+    if len(x)>1  and  '{' not in x:
+        x=re.findall(r'#[a-fA-F0-9]{3,6}',s)
+        [print(i) for  i in x]
